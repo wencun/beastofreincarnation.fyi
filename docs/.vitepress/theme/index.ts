49aaad94e8a3eb2ad86be3e4d97c6cac5ad3b9a7
wencun <wencun@users.noreply.github.com>
2026-08-06 15:35:47 +0800

    Display analytics consent control

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,3 +1,11 @@
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
+import TrackingConsent from './TrackingConsent.vue'
 import './custom.css'
-export default DefaultTheme
+
+export default {
+  extends: DefaultTheme,
+  Layout: () => h(DefaultTheme.Layout, null, {
+    'layout-bottom': () => h(TrackingConsent)
+  })
+}
