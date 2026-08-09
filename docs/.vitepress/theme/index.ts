29bacf43bfb0dfb739528903f217cd2562eea37f
wencun <wencun@users.noreply.github.com>
2026-08-09 20:16:10 +0800

    Render guide ad unit on article pages

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,16 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
+import GuideAd from './GuideAd.vue'
 import TrackingConsent from './TrackingConsent.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('GuideAd', GuideAd)
+  },
   Layout: () => h(DefaultTheme.Layout, null, {
+    'doc-after': () => h(GuideAd),
     'layout-bottom': () => h(TrackingConsent)
   })
 }
