a40f50a02509feef098edf7e92aecafb38ac41de
wencun <wencun@users.noreply.github.com>
2026-08-18 16:10:14 +0800

    Use responsive banner ad on home page

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,14 +1,13 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import AdsterraContentAd from './AdsterraContentAd.vue'
-import AdsterraHomeAd from './AdsterraHomeAd.vue'
 import TrackingConsent from './TrackingConsent.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
   Layout: () => h(DefaultTheme.Layout, null, {
-    'home-hero-before': () => h(AdsterraHomeAd),
+    'home-hero-before': () => h(AdsterraContentAd, { placement: 'top' }),
     'doc-before': () => h(AdsterraContentAd, { placement: 'top' }),
     'doc-after': () => h(AdsterraContentAd, { placement: 'bottom' }),
     'layout-bottom': () => h(TrackingConsent)
