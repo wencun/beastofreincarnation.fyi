3e5048f1bbb9a26f414321c9a81a8bf35bc9b8e5
wencun <wencun@users.noreply.github.com>
2026-08-23 12:14:29 +0800

    feat: replace Adsterra placements with AdSense

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,15 +1,15 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
-import AdsterraContentAd from './AdsterraContentAd.vue'
+import AdSenseContentAd from './AdSenseContentAd.vue'
 import TrackingConsent from './TrackingConsent.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
   Layout: () => h(DefaultTheme.Layout, null, {
-    'home-hero-before': () => h(AdsterraContentAd, { placement: 'home' }),
-    'doc-before': () => h(AdsterraContentAd, { placement: 'top' }),
-    'doc-after': () => h(AdsterraContentAd, { placement: 'bottom' }),
+    'home-hero-before': () => h(AdSenseContentAd, { placement: 'home' }),
+    'doc-before': () => h(AdSenseContentAd, { placement: 'top' }),
+    'doc-after': () => h(AdSenseContentAd, { placement: 'bottom' }),
     'layout-bottom': () => h(TrackingConsent)
   })
 }
